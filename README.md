84278e05f769270902bab5a6fc76e8c6b5939d90
wshen24 <shenweihai1@gmail.com>
2019-03-06 20:04:05 -0700

    update

---
+++ README.md
@@ -0,0 +1,28 @@
+### Face_detection
+using the pre-trained model from face_alignment to recognize the face object from the input image based on [How far are we from solving the 2D \& 3D Face Alignment problem](https://adrianbulat.com/face-alignment)
+
+
+
+### installation
+```
+conda install -c 1adrianb face_alignment --name=conda_envs
+conda install PIL --name=conda_envs
+```
+
+### data-set
+LS3D-W is a large-scale 3D face alignment dataset constructed by annotating the images from AFLW[2], 300VW[3], 300W[4] and FDDB[5] in a consistent manner with 68 points using the automatic method described in [1]. To gain access to the dataset please enter your email address in the following form. You will shortly receive an email at the specified address containing the download link
+``` 
+References: 
+[1] A. Bulat, G. Tzimiropoulos. How far are we from solving the 2D & 3D Face Alignment problem? (and a dataset of 230,000 3D facial landmarks), arxiv, 2017 
+[2] M. Kostinger, P. Wohlhart, P.M. Roth, and H. Bischof. Annotated facial landmarks in the wild: A large-scale, real-world database for facial landmark localization, In ICCVW, 2011 
+[3] J. Shen, S. Zafeiriou, G. G. Chrysos, J. Kossaifi, G. Tzimiropoulos, and M. Pantic. The first facial landmark tracking in-the-wild challenge: Benchmark and results. In ICCVW, 2015 
+[4] C. Sagonas, G. Tzimiropoulos, S. Zafeiriou, and M. Pantic. 300 faces in-the-wild challenge: The first facial landmark localization challenge. In ICCVW, 2013 
+[5] V. Jain, E. Learned-Miller FDDB: A Benchmark for Face Detection in Unconstrained Settings. UMass Amherst Technical Report, 2010
+```
+
+### tips
+When try to function `get_landmarks_from_image` to predict:
+>
+> AttributeError: module 'torch.nn.functional' has no attribute 'interpolate'
+
+refer to [[commit](https://github.com/Skorkmaz88/face-alignment/commit/7ca0f9eaa8020110d529b81a6853a6c01e672472)]
---
+++ detector.py
@@ -0,0 +1,62 @@
+from __future__ import print_function, division
+import face_alignment
+from skimage import io
+import numpy as np
+from PIL import Image
+import warnings
+from os.path import abspath, dirname, join
+BASE_PATH = abspath(dirname(abspath(__file__)))
+warnings.filterwarnings("ignore")
+STATUS = {
+    1: join(BASE_PATH, 'emoji/h.jpg'),  # happy
+    2: join(BASE_PATH, 'emoji/s.jpg'),  # sad
+    3: join(BASE_PATH, 'emoji/n.jpg'),  # neutral
+}
+
+
+class Detector(object):
+    def __init__(self, device="cpu"):
+        # download the weights model when first time
+        self.face = face_alignment.FaceAlignment(face_alignment.LandmarksType._2D, flip_input=False, device=device)
+
+    def replacement(self, input_image, out_image, status):
+        if status not in STATUS:
+            return False, "status is valid"
+        try:
+            emotion_image = STATUS[status]
+
+            X = io.imread(input_image)
+            landmarks = self.face.get_landmarks_from_image(X)[-1]
+            box_width = min(np.max(landmarks[:, 0]) - np.min(landmarks[:, 0]), np.max(landmarks[:, 1]) - np.min(landmarks[:, 1]))
+
+            input_image_rev = Image.open(input_image)
+            emotion_image_rev = Image.open(emotion_image)
+            wc, hc = emotion_image_rev.size
+            if wc != hc:
+                raise Exception(emotion_image + " must have the same width and height")
+
+            zoom = box_width / (wc + 0.0)
+            emotion_image_rev_scale = emotion_image_rev.resize((int(wc * zoom), int(wc*zoom)))
+            offset = (int(np.mean(landmarks[:, 0]) - 0.5 * (int(wc * zoom))), int(np.mean(landmarks[:, 1]) - 0.5 * (int(wc * zoom))))
+            input_image_rev.paste(emotion_image_rev_scale, offset)
+            input_image_rev.save(out_image)
+        except Exception as ex:
+            return False, str(ex)
+        return True, ""
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+    import random
+    obj = Detector()
+
+    for img in ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg"]:
+        input_image = join(BASE_PATH, "assets", img)
+        out_image = join(BASE_PATH, "assets", "out_" + img)
+        status, msg = obj.replacement(input_image, out_image, random.randint(1, 3))
+
+        if status:
+            plt.imshow(io.imread(out_image))
+            plt.pause(10)
+        else:
+            print(msg)
